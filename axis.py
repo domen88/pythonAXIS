9f025676ccc7dd4b3d0e0916cd8f17fb40b7078e
Domenico Scotece <domen.88@live.it>
2017-09-29 12:01:32 +0200

    fix name file

--- axis.py
+++ axis.py
@@ -13,8 +13,8 @@ def video(hostname):
     #start record
     url = 'http://' + hostname + '/axis-cgi/mjpg/video.cgi?duration=60'
     ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H:%M:%S')
-    file_name = 'video-' + st + '.mjpg'
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M')
+    file_name = 'v_' + st + '.mjpg'
     path = '/mnt/' + file_name
     wget.download(url, path)
 
